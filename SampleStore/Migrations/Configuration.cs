169d911c5467c18cdcf0d3194370e1e66041f004
JV Pabalan <jvv.pabalan@outlook.com>
2014-02-15 15:59:29 +0800

    Add new Products for seeding the database

--- SampleStore/Migrations/Configuration.cs
+++ SampleStore/Migrations/Configuration.cs
@@ -15,7 +15,14 @@ namespace SampleStore.Migrations
 
         protected override void Seed(SampleStore.Models.StoreContext context)
         {
-            context.Products.AddOrUpdate(p => p.Name, new Product { Name = "PlayStation 4", Category = "Gaming", Price = 399M });
+            context.Products.AddOrUpdate(p => p.Name,
+                new Product[] { 
+                    new Product { Name = "PlayStation 4", Price = 399M, Category = "Gaming"},
+                    new Product { Name = "Xbox One", Price = 499M, Category = "Gaming" },
+                    new Product { Name = "PlayStation Vita", Price = 169M, Category = "Gaming"}
+                }
+                
+                );
             context.SaveChanges();
         }
     }
