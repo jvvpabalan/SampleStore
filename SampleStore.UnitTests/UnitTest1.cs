9f3ad0d77ce9d151ee209b990450c07ab0568c7e
JV Pabalan <jvv.pabalan@outlook.com>
2014-03-11 23:22:25 +0800

    Add new version of web api and add Unit Tests to product controller

--- SampleStore.UnitTests/UnitTest1.cs
+++ SampleStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SampleStore.Models;
+using System.Linq;
+using SampleStore.Controllers;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace SampleStore.UnitTests
 {
@@ -7,8 +13,26 @@ namespace SampleStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Give_Products()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            mock.Setup(m => m.GetAll()).Returns(new Product[] 
+                {
+                    new Product { Id = 1, Name = "Prod1"},
+                    new Product { Id = 2, Name = "Prod2"},
+                    new Product { Id=3, Name = "Prod3"},
+                    new Product { Id= 4, Name = "Prod4"},
+                    new Product{ Id=5, Name="Prod5"}
+                }.AsEnumerable());
+
+            ProductsController controller = new ProductsController(mock.Object);
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.GetAllProducts();
+
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 5);
+            Assert.AreEqual(prodArray[0].Name, "Prod1");
+            Assert.AreEqual(prodArray[4].Name, "Prod5");
         }
     }
 }
