f55a423831e45f3fdefc13cc03771ce18082384b
JV Pabalan <jvv.pabalan@outlook.com>
2014-03-11 18:55:10 +0800

    Modify global.asax to use NinjectControllerFactory for creating controllers

--- SampleStore/Global.asax.cs
+++ SampleStore/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SampleStore.Infrastracture;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,8 @@ namespace SampleStore
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
