797d36cd229c02a6e05b55a6063caae80e115bd3
JV Pabalan <jvv.pabalan@outlook.com>
2014-03-12 13:51:54 +0800

    Add Unit test for adding products

--- SampleStore.UnitTests/UnitTest1.cs
+++ SampleStore.UnitTests/UnitTest1.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using SampleStore.Controllers;
 using System.Collections;
 using System.Collections.Generic;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Hosting;
 
 namespace SampleStore.UnitTests
 {
@@ -33,6 +36,31 @@ namespace SampleStore.UnitTests
             Assert.IsTrue(prodArray.Length == 5);
             Assert.AreEqual(prodArray[0].Name, "Prod1");
             Assert.AreEqual(prodArray[4].Name, "Prod5");
+        }
+
+        [TestMethod]
+        public void Can_Add_Product()
+        {
+            var mock = CreateMockObject();
+            var product = new Product{ Id = 1, Name = "Prod1", Category = "Gaming", Price = 255};
+            var config = new HttpConfiguration();
+            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost/api/products/1");
+            var route = config.Routes.MapHttpRoute("DefaultApi", "api/products/{id}");
+            ProductsController controller = new ProductsController(mock.Object);
+            controller.Request = request;
+            controller.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = config;
+            controller.Request.Properties[HttpPropertyKeys.HttpRouteDataKey] = route;
+            var result = controller.PostProduct(product);
+            
+            mock.Verify(m => m.Add(It.IsAny<Product>()), Times.Once());
+            Assert.AreEqual(System.Net.HttpStatusCode.Created, result.StatusCode);
+
+        }
+
+        private Mock<IProductRepository> CreateMockObject()
+        {
+            return new Mock<IProductRepository>();
+
         }
     }
 }
